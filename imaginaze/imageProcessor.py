569715c15725fabb356d9ee070786bf643a06d81
Roma <bshs71734@gmail.com>
2024-12-09 02:14:31 +0300

    Update imageProcessor.py

--- imaginaze/imageProcessor.py
+++ imaginaze/imageProcessor.py
@@ -1,1 +1,14 @@
+from PIL import ImageFilter, ImageOps
+
+class ImageProcessor:
+    def __init__(self, img):
+        self.img = img  
+
+    def sharpen(self):
+        self.img = self.img.filter(ImageFilter.SHARPEN)  # фильтр резкости
+        return self.img  
+
+    def border(self):
+        self.img = ImageOps.expand(self.img, border=15, fill='black')  # рамка 15 ph
+        return self.img 
 
