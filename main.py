5e5334168b58cbe6ca055c8ae6c436a6c3417f54
roma <166542724+petuxonchik@users.noreply.github.com>
2024-12-09 15:20:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from imaginaze.imageHandler import ImageHandler
+from imaginaze.imageFunction import Imagine
+
 from PIL import Image
 import tkinter as tk
 
@@ -9,7 +11,7 @@ class Application(tk.Tk): # класс наследует tkinter
         self.title('Работа с фото') # титульник дял программы
         first_label = tk.Label(self, text="Работа с фото", font=10) #   лейбл для программы
         first_label.pack(pady=2, padx=2) # размещение лейбла в окне
-        self.image_handler = ImageHandler(master=self) # создание обьекта класса
+        self.image_handler = Imagine(master=self) # создание обьекта класса
         self.image_handler.pack(pady=5, padx=5) # расмещение окна обьекта в главном  окне программы
 
         
